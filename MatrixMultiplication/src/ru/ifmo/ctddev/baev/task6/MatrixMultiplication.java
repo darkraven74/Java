12a91393d8e45d3edd2bf2c035aaaf1934eabf82
Vladimir Baev <darkraven8@gmail.com>
2013-05-09 15:38:31 +0400

    stable version

--- MatrixMultiplication/src/ru/ifmo/ctddev/baev/task6/MatrixMultiplication.java
+++ MatrixMultiplication/src/ru/ifmo/ctddev/baev/task6/MatrixMultiplication.java
@@ -19,6 +19,12 @@ public class MatrixMultiplication {
 	private static int[][] matrixB;
 	/** matrix anwser. */
 	private static int[][] matrixAnwser;
+	/** time of calculating. */
+	private static double workTime;
+
+	public static double getWorkTime() {
+		return workTime;
+	}
 
 	/**
 	 * Initializes 2 matrices with random numbers.
@@ -38,8 +44,11 @@ public class MatrixMultiplication {
 
 	/**
 	 * Calculates <code>i</code>, <code>j</code> cell of {@link #matrixAnwser}.
-	 * @param i index of row.
-	 * @param j index of column.
+	 * 
+	 * @param i
+	 *            index of row.
+	 * @param j
+	 *            index of column.
 	 */
 	private static void calculateCell(int i, int j) {
 		int anwser = 0;
@@ -48,9 +57,10 @@ public class MatrixMultiplication {
 		}
 		matrixAnwser[i][j] = anwser;
 	}
-	
+
 	/**
 	 * Calculates sum of elements in {@link #matrixAnwser}.
+	 * 
 	 * @return Sum of elements in {@link #matrixAnwser}.
 	 */
 	private static int calculateSum() {
@@ -62,13 +72,31 @@ public class MatrixMultiplication {
 		}
 		return anwser;
 	}
-	
-	private class MyRunnable implements Runnable {
-		private int curPos, cellsInThread;
+
+	/**
+	 * Class MyRunnable calculates data in thread.
+	 * 
+	 * @author Vladimir Baev
+	 * 
+	 */
+	private static class MyRunnable implements Runnable {
+		/** start position to calculating. */
+		private int curPos;
+		/** number of cells in thread. */
+		private int cellsInThread;
+
+		/**
+		 * 
+		 * @param curPos
+		 *            start position to calculating.
+		 * @param cellsInThread
+		 *            number of cells in thread.
+		 */
 		public MyRunnable(int curPos, int cellsInThread) {
 			this.curPos = curPos;
 			this.cellsInThread = cellsInThread;
 		}
+
 		@Override
 		public void run() {
 			for (int i = 0; i < cellsInThread; i++) {
@@ -83,7 +111,7 @@ public class MatrixMultiplication {
 	 * 
 	 * @param args
 	 *            arguments of command line (number of matrices and threads).
-	 *    
+	 * 
 	 */
 	public static void main(String[] args) {
 		if (args.length < 2) {
@@ -101,20 +129,31 @@ public class MatrixMultiplication {
 
 		matrixInit();
 
-		int cellsInThread = (int)Math.floor(1.0 * n * n / m);
+		int cellsInThread = (int) Math.floor(1.0 * n * n / m);
 		int cellsInLastThread = n * n - cellsInThread * (m - 1);
 		int curPos = 0;
-		Thread[] threads;
-		
+		Thread[] threads = new Thread[m];
+
+		long startTime = System.nanoTime();
+
 		for (int i = 0; i < m - 1; i++) {
-			createThread(curPos, cellsInThread);
+			threads[i] = new Thread(new MyRunnable(curPos, cellsInThread));
+			threads[i].start();
 			curPos += cellsInThread;
 		}
-		createThread(curPos, cellsInLastThread);
-		
-		
-		
-		System.out.println(calculateSum());
-	}
+		threads[m - 1] = new Thread(new MyRunnable(curPos, cellsInLastThread));
+		threads[m - 1].start();
 
+		for (int i = 0; i < m; i++) {
+			try {
+				threads[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		workTime = (System.nanoTime() - startTime) / 1e6;
+		System.out.println("Time: " + workTime);
+		System.out.println("Sum: " + calculateSum());
+	}
 }
