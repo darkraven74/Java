d0abaaf51a6f9f3d9f8ba127c243bf760d64be58
Vladimir Baev <darkraven8@gmail.com>
2013-05-22 17:25:50 +0400

    small changes

---
+++ Client/src/ru/ifmo/ctddev/baev/task7/Test.java
@@ -0,0 +1,15 @@
+package ru.ifmo.ctddev.baev.task7;
+
+import java.util.Random;
+
+public class Test {
+	public static void main(String[] args) {
+		Random r1 = new Random();
+		System.out.println(r1.nextInt(100));
+		System.out.println(r1.nextInt(100));
+		
+		Random r2 = new Random();
+		System.out.println(r2.nextInt(100));
+		System.out.println(r2.nextInt(100));
+	}
+}
